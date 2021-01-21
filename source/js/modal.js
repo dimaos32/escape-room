640ded61696fe512b5da09564479cc66b89439a0
Dima Os <dimaos@protonmail.com>
2021-01-21 15:08:55 +0300

    Выполняет окно обратной связи

--- source/js/modal.js
+++ source/js/modal.js
@@ -4,8 +4,13 @@
   var locationLink = document.querySelector('.site-nav__link--location');
   var location = document.querySelector('.modal--location');
   var locationCloseBtn = location.querySelector('.modal__close-btn');
+  var feedbackLink = document.querySelector('.site-nav__link--question');
+  var feedback = document.querySelector('.modal--feedback');
+  var feedbackCloseBtn = feedback.querySelector('.modal__close-btn');
 
   var locationModal = new window.utils.Modal(location, locationCloseBtn);
+  var feedbackModal = new window.utils.Modal(feedback, feedbackCloseBtn);
 
   locationLink.addEventListener('click', locationModal.onModalLinkClick);
+  feedbackLink.addEventListener('click', feedbackModal.onModalLinkClick);
 })();
