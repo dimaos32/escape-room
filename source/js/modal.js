09046ea5e7fb59427c9f4bc215af2f4f480d5096
Dima Os <dimaos@protonmail.com>
2021-02-01 15:00:19 +0300

    fix: cod review 79

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,16 +1,23 @@
 'use strict';
 
 (function () {
-  var locationLink = document.querySelector('.site-nav__link--location');
-  var location = document.querySelector('.modal--location');
-  var locationCloseBtn = location.querySelector('.modal__close-btn');
-  var feedbackLink = document.querySelector('.site-nav__link--question');
-  var feedback = document.querySelector('.modal--feedback');
-  var feedbackCloseBtn = feedback.querySelector('.modal__close-btn');
+  if (document.querySelector('.modal--location')) {
+    var locationLink = document.querySelector('.site-nav__link--location');
+    var location = document.querySelector('.modal--location');
+    var locationCloseBtn = location.querySelector('.modal__close-btn');
 
-  var locationModal = new window.utils.Modal(location, locationCloseBtn);
-  var feedbackModal = new window.utils.Modal(feedback, feedbackCloseBtn);
+    var locationModal = new window.utils.Modal(location, locationCloseBtn);
 
-  locationLink.addEventListener('click', locationModal.onModalLinkClick);
-  feedbackLink.addEventListener('click', feedbackModal.onModalLinkClick);
+    locationLink.addEventListener('click', locationModal.onModalLinkClick);
+  }
+
+  if (document.querySelector('.modal--feedback')) {
+    var feedbackLink = document.querySelector('.site-nav__link--question');
+    var feedback = document.querySelector('.modal--feedback');
+    var feedbackCloseBtn = feedback.querySelector('.modal__close-btn');
+
+    var feedbackModal = new window.utils.Modal(feedback, feedbackCloseBtn);
+
+    feedbackLink.addEventListener('click', feedbackModal.onModalLinkClick);
+  }
 })();
